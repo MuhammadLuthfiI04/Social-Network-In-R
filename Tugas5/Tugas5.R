e70a4d7fa5e4439bd62a7c4c25f07a851bbe17de
Muhammad Luthfi Ichsanudin <67815792+MuhammadLuthfiI04@users.noreply.github.com>
2021-08-12 15:27:27 +0700

    Add files via upload

---
+++ Tugas5/Tugas5.R
@@ -0,0 +1,12 @@
+# Menghitung nilai akhir
+
+nama <- "Muhammad Luthfi Ichsanudin"
+nim <- "A11.2019.12195"
+matakuliah <- "Social Network In R"
+nilai_tugas <- 8.5
+nilai_uts <- 8.5
+nilai_uas <- 9.3
+nilai_akhir <- (nilai_tugas + nilai_uts + nilai_uas)/3
+
+datanilai <- data.frame(nama, nim, matakuliah, nilai_akhir)
+datanilai
